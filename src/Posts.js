9e1407c71d6a1c578607e6596ff6ee4836a52e1c
Naamafb <naamafb12@gmail.com>
2023-05-16 19:32:11 +0300

    fetch in posts

--- src/Posts.js
+++ src/Posts.js
@@ -1,6 +1,32 @@
- function Posts(){
+import React, { useState, useEffect } from 'react';
+
+function Posts() {
+  let jsonUser = localStorage.getItem("user");
+  let user = JSON.parse(jsonUser);
+  let userid = user.id;
+
+  const [posts, setPosts] = useState([]);
+  const [findPosts, setFindPosts] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userid}`)
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch(() => setFindPosts(false));
+  }, [userid]);
+
+  if (findPosts) {
+    let postsHtml = posts.map((post) => <p key={post.id}>{post.title}</p>);
     return (
-        <p>posts</p>
-    )
+      <div>
+        {postsHtml}
+      </div>
+    );
+  }
+
+  return (
+    <h2>There are no posts</h2>
+  );
 }
+
 export default Posts;
