500fe77cd50a8688edeb4f0a526cc4dd5185b1e6
NoaShimshi <noanoashimshi@gmail.com>
2023-05-22 13:12:42 +0300

    a

--- src/Photos.js
+++ src/Photos.js
@@ -1,66 +1,84 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link, useParams } from 'react-router-dom';
- 
- function Photos(){
-    let jsonUser = localStorage.getItem("user");
-    let user = JSON.parse(jsonUser);
-    let userid = user.id;
-    let {albumId} = useParams();
-    const [photos, setPhotos] = useState([]);
-    const [findPhotos, setFindPhotos] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
-    const photosContainerRef = useRef(null);
-  
-    useEffect(() => {
-      fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
-        .then((response) => response.json())
-        .then((data) => setPhotos(data))
-        .catch(() => setFindPhotos(false));
-    },[userid, albumId]);
 
-    useEffect(() => {
-        const handleScroll = () => {
-          const container = photosContainerRef.current;
-          const containerHeight = container.offsetHeight;
-          const scrollTop = container.scrollTop;
-          const scrollHeight = container.scrollHeight;
-    
-          if (scrollHeight - scrollTop === containerHeight) {
-            loadMorePhotos();
-          }
-        };
-    
-        const loadMorePhotos = () => {
-          // Simulating loading more photos
-          setIsLoading(true);
-          setTimeout(() => {
-            // Add more photos to the existing list or fetch from API
-            setIsLoading(false);
-          }, 1000);
-        };
-    
-        photosContainerRef.current.addEventListener('scroll', handleScroll);
-    
-        return () => {
-          photosContainerRef.current.removeEventListener('scroll', handleScroll);
-        };
-      }, [photos]);
+function Photos() {
+  let jsonUser = localStorage.getItem('user');
+  let user = JSON.parse(jsonUser);
+  let userid = user.id;
+  let { albumId } = useParams();
+  const [photos, setPhotos] = useState([]);
+  const [findPhotos, setFindPhotos] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const photosContainerRef = useRef(null);
 
-    if (findPhotos) {
-        let photosHtml = photos.map((photo)=>
-        (<div key={photo.id}>
-            <img alt={photo.title} src={photo.thumbnailUrl}/>
+  useEffect(() => {
+    fetchPhotos();
+  }, [userid, albumId]);
+
+  const fetchPhotos = () => {
+    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}&_page=${page}&_limit=8`)
+      .then((response) => {
+        const totalCount = response.headers.get('x-total-count');
+        setHasMore(totalCount > page * 8);
+        return response.json();
+      })
+      .then((data) => {
+        setPhotos((prevPhotos) => [...prevPhotos, ...data]);
+        setIsLoading(false);
+      })
+      .catch(() => setFindPhotos(false));
+  };
+
+  const handleScroll = () => {
+    const container = photosContainerRef.current;
+    const containerHeight = container.offsetHeight;
+    const scrollTop = container.scrollTop;
+    const scrollHeight = container.scrollHeight;
+
+    if (scrollHeight - scrollTop - containerHeight <= 10 && !isLoading && hasMore) {
+      setIsLoading(true);
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    const container = photosContainerRef.current;
+    if (container) {
+      container.addEventListener('scroll', handleScroll);
+
+      return () => {
+        container.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    setPhotos([]);
+    setPage(1);
+    setHasMore(true);
+    fetchPhotos();
+  }, [albumId]);
+
+  if (findPhotos) {
+    let photosHtml = photos.map((photo) => (
+      <div key={photo.id} style={{ margin: '10px' }}>
+        <img alt={photo.title} src={photo.thumbnailUrl} style={{ maxWidth: '100%', height: 'auto' }} />
+      </div>
+    ));
+
+    return (
+      <div ref={photosContainerRef} style={{ height: '100vh', overflowY: 'scroll' }}>
+        <div style={{ minHeight: '100%' }}>
+          {photosHtml}
         </div>
-        ));
-
-        return (
-            <div ref={photosContainerRef} style={{ height: '300px', overflowY: 'scroll' }}>
-                {photosHtml}
-                {isLoading && <div>Loading more photos...</div>}
-            </div>
+        {isLoading && <div>Loading more photos...</div>}
+      </div>
     );
   }
 
   return <h2>There are no photos</h2>;
 }
+
 export default Photos;
